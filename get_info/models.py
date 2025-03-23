65e2d0038025e9259e10bd47a2f9ae4b7699596d
vishnumanikandan <vishnumanikandan2006@gmail.com>
2025-03-23 15:11:24 +0530

    adding command line

--- get_info/models.py
+++ get_info/models.py
@@ -14,3 +14,6 @@ class User(models.Model):
     user_name = models.CharField(unique=True,max_length=40)
     password = models.CharField(max_length=40)
     role = models.CharField(role=USER_ROLE,default='user')
+
+
+#from first commit 
