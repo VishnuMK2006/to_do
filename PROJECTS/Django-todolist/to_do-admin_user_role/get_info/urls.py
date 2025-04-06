5f60a93d9ab2cd00d83e6c7499ca0e320a9a84bc
shubha <shubhashree1196@gmail.com>
2025-04-06 12:13:29 +0530

    Features added: Urgency, File Uploading

---
+++ PROJECTS/Django-todolist/to_do-admin_user_role/get_info/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+urlpatterns=[
+    path('admin_task_assignment/<int:user_id>/',views.admin_user_list,name='admin_user_list'),
+    path('t_submit/<int:user_id>/',views.assign_task,name='assign_task'),
+    path('signup/',views.signup_area,name='signup_area'),
+    path('login/',views.login_area,name='login_area'),
+    path('task-assign/<int:user_id>/', views.task_assign, name='task_assign'),
+    path('task-complete/<int:user_id>/', views.mark_completed, name='mark_completed'),
+]
